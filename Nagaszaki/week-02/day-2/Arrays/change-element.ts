87864d9098c7c157acf520d1eb2e82b20f2ed878
Nagaszaki <nagabor99@gmail.com>
2021-04-14 14:41:23 +0200

    Shortened .map object

--- Nagaszaki/week-02/day-2/Arrays/change-element.ts
+++ Nagaszaki/week-02/day-2/Arrays/change-element.ts
@@ -5,8 +5,6 @@
 // -  Print the fourth element as a test
 
 let numbers: number [] = [1, 2, 3, 8, 5, 6];
-let changer = numbers.map(function(){
-    return numbers[3]/2;
-});
+let changer = numbers.map(x => x / 2);
 numbers[3] = changer[3];
 console.log(numbers[3]);
