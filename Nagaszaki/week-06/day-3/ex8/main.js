390a19991b92c7cbf8127420aa07003cb6a5af07
Nagaszaki <nagabor99@gmail.com>
2021-05-19 17:12:15 +0200

    JS creates an article for each child and their pets

---
+++ Nagaszaki/week-06/day-3/ex8/main.js
@@ -0,0 +1,26 @@
+const kids = [
+  { 'petName': 'Wattled crane', 'owner': 'Bryant' },
+  { 'petName': 'Devil, tasmanian', 'owner': 'Alejandro' },
+  { 'petName': 'Mynah, common', 'owner': 'Nelie' },
+  { 'petName': 'Dolphin, common', 'owner': 'Mariele' },
+  { 'petName': 'Gray duiker', 'owner': 'Maddalena' },
+  { 'petName': 'Crab (unidentified)', 'owner': 'Lucine' },
+  { 'petName': 'Ant (unidentified)', 'owner': 'Lorianna' },
+  { 'petName': 'Bison, american', 'owner': 'Tommie' },
+  { 'petName': 'Yellow mongoose', 'owner': 'Vivyan' },
+  { 'petName': 'Carpet snake', 'owner': 'Veda' },
+  { 'petName': 'Lesser mouse lemur', 'owner': 'Isidor' },
+];
+
+const main = document.querySelector('main');
+
+kids.forEach( x => {
+  const newItem = document.createElement('article');
+  const newH = document.createElement('h3');
+  const newP = document.createElement('p');
+  newH.textContent = x.owner;
+  newP.textContent = x.petName;
+  newItem.appendChild(newH);
+  newItem.appendChild(newP);
+  main.appendChild(newItem);
+})
