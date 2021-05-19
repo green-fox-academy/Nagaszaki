b196cf1a8ee03d8145ddfe5234ed229b5f9b153d
Nagaszaki <nagabor99@gmail.com>
2021-05-19 14:11:53 +0200

    JS reads out king buisenesslamp and logs them

---
+++ Nagaszaki/week-06/day-3/ex1/main.js
@@ -0,0 +1,9 @@
+let king = document.getElementById('b325');
+console.log(king);
+
+let businessLamp = document.querySelectorAll('.asteroid.big');
+console.log(businessLamp[0].innerHTML);
+console.log(businessLamp[1].innerHTML);
+
+let conceitedKing = document.querySelector('.asteroid').innerHTML+'\n'+document.querySelector('.asteroid.b326').innerHTML;
+alert(conceitedKing);
