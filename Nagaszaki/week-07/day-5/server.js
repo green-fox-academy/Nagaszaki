201e081e08db8c07c6e950420b6234014ad209de
Nagaszaki <nagabor99@gmail.com>
2021-05-28 17:03:04 +0200

    Added server.js that has get and put endpoints

---
+++ Nagaszaki/week-07/day-5/server.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+app.use(express.json())
+app.use(express.static('public'))
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'reddit',
+});
+
+conn.connect((err) => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('connected');
+  }
+})
+
+app.get('/hello', (req,res) => {
+  res.send('hello world');
+})
+
+app.get('/posts', (reg,res) => {
+  conn.query('SELECT * FROM posts', (err, row) => {
+    if (err) {
+      res.status(500).json(err);
+    } 
+    res.status(200).json(row);
+  })
+})
+
+app.post('/posts', (req,res) => {
+  conn.query('INSERT INTO posts (title, url, timestamp, score) VALUES (?,?,?,0);',
+  [req.body.title,req.body.url,req.body.timestamp], (err, result) => {
+    if (err) {
+      res.status(500).json(err);
+    }
+    res.status(200).json(result);
+  }
+)})
+
+app.put('/posts/:id/upvote', (req,res) => {
+  conn.query('UPDATE posts SET score = score+1 WHERE id = ?;', [req.params.id], (err,rows) => {
+    if (err) {
+      console.log(err.toString());
+
+      res.status(500).json(err);
+    }
+    res.status(200).json({"message" : "Updated the record succesfully"});
+  }
+  )
+})
+
+
+app.put('/posts/:id/downvote', (req,res) => {
+  conn.query('UPDATE posts SET score = score-1 WHERE id = ?;', [req.params.id], (err,rows) => {
+    if (err) {
+      console.log(err.toString());
+
+      res.status(500).json(err);
+    }
+    res.status(200).json({"message" : "Updated the record succesfully"});
+  }
+  )
+})
+
+app.listen(3000);
