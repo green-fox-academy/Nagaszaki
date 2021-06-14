edc1b213568e44785c7ebd7e35d5989626c3dc3c
Nagaszaki <nagabor99@gmail.com>
2021-06-14 14:07:12 +0200

    Basic structure for music player is finished, added audi/image files, connected all files

---
+++ Nagaszaki/week-10/MusicPlayer/public/index.js
@@ -0,0 +1,17 @@
+const audio = document.querySelector('audio');
+
+audio.onloadstart = () =>{
+  console.log('onloadstart happened');
+}
+
+audio.onplay = () =>{
+  console.log('onplay happened');
+}
+
+audio.onended = () =>{
+  console.log('onended happened');
+}
+
+audio.onprogress = () => {
+  console.log('onprogress happened');
+}
